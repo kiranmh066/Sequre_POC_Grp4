c7e61fb023ae99687740082a9a69ed31e2476a5b
Bhoomika <Bhoomika.CM@Valtech.com>
2023-03-24 15:25:24 +0530

    MVC LAYER

---
+++ Office_Seat_Booking/Office_Seat_Booking_MVC/Controllers/FloorController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Office_Seat_Booking_MVC.Controllers
+{
+    public class FloorController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Office_Seat_Booking/Office_Seat_Booking_MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Office_Seat_Booking_MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
